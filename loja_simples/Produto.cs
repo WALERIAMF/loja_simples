cffcb141f20274f72eec27365975473ad7ca5dff
waleriamf <waleriamf@gmail.com>
2021-02-08 22:47:28 -0300

    Versao simples

---
+++ loja_simples/Produto.cs
@@ -0,0 +1,81 @@
+﻿using System.Globalization;
+
+
+namespace loja_simples
+{
+    class Produto
+    {
+        public string _nome;
+        public double Preco { get; private set; }
+        private int Quantidade { get; set; }
+
+        public Produto()
+        { }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public string preco
+        {
+            get { return preco; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    preco = value;
+                }
+            }
+        }
+
+        public string quantidade
+        {
+            get { return quantidade; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    quantidade = value;
+                }
+            }
+        }
+        public double ValorTotalEmEstoque
+        {
+            get { return Preco * Quantidade; }
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $"
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $"
+            + ValorTotalEmEstoque.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+
+}
+
---
+++ loja_simples/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace loja_simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Quantidade no estouque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados inseridos: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " +p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine("Dados atalizados: " + p);
+            /* double? x = null;
+             double? y = 10.0;
+
+             Console.WriteLine(x.GetValueOrDefault());
+             Console.WriteLine(y.GetValueOrDefault());
+
+             Console.WriteLine(x.HasValue);
+             Console.WriteLine(y.HasValue);
+
+             if(x.HasValue)
+                 Console.WriteLine(x.Value);
+             else
+                 Console.WriteLine("X is null");
+             if(y.HasValue)
+                 Console.WriteLine(y.Value);
+             else
+                 Console.WriteLine("Y is null");
+ */
+        }
+    }
+}
